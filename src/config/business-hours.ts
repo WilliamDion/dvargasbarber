7a1bda17fd333fca46f19fa753938d15b31a60f4
WilliamDion <williamsantos350@gmail.com>
2026-03-18 17:53:33 -0300

    Update business-hours.ts

--- src/config/business-hours.ts
+++ src/config/business-hours.ts
@@ -1,10 +1,10 @@
 export const BUSINESS_HOURS = {
   // 0 = Domingo, 1 = Segunda, ..., 6 = Sábado
-  1: { open: "08:00", close: "20:00", lunchStart: "12:00", lunchEnd: "13:30" },
-  2: { open: "08:00", close: "20:00", lunchStart: "12:00", lunchEnd: "13:30" },
-  3: { open: "08:00", close: "20:00", lunchStart: "12:00", lunchEnd: "13:30" },
-  4: { open: "08:00", close: "20:00", lunchStart: "12:00", lunchEnd: "13:30" },
-  5: { open: "08:00", close: "20:00", lunchStart: "12:00", lunchEnd: "13:30" },
-  6: { open: "08:00", close: "20:00", lunchStart: "12:00", lunchEnd: "13:00" },
+  1: { open: "06:00", close: "23:30", lunchStart: "13:00", lunchEnd: "13:30" },
+  2: { open: "06:00", close: "23:30", lunchStart: "13:00", lunchEnd: "13:30" },
+  3: { open: "06:00", close: "23:30", lunchStart: "13:00", lunchEnd: "13:30" },
+  4: { open: "06:00", close: "23:30", lunchStart: "13:00", lunchEnd: "13:30" },
+  5: { open: "06:00", close: "23:30", lunchStart: "13:00", lunchEnd: "13:30" },
+  6: { open: "06:00", close: "23:30", lunchStart: "13:00", lunchEnd: "13:30" },
   0: { open: null, close: null, label: "Domingo: Fechado" }, // Fechado
 };
